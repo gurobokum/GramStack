01b2309e03a7586bef4654390c44bcd6706c89d9
Guro Bokum <jiojiajiu@gmail.com>
2026-09-20 15:41:51 +0300

    fix(backend): enable logging in worker

--- packages/backend/app/worker/__init__.py
+++ packages/backend/app/worker/__init__.py
@@ -1,10 +1,10 @@
 import app.tasks  # noqa
 import app.credits.tasks  # noqa
 import app.models.all  # noqa
-# from app.logging import configure_logging
+from app.logging import configure_logging
 
 from .conf import WorkerSettings, task
 
-# configure_logging("worker")
+configure_logging(name="worker")
 
 __all__ = ["WorkerSettings", "task"]
